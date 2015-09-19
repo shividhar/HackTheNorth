62054297c8a9dbb7870e2308904f01030f2a17f0
AY MB <assafyossifoff@v1021-wn-33-228.campus-dynamic.uwaterloo.ca>
2015-09-19 14:22:17 -0400

    lord knows what changed

--- hackthenorth.js
+++ hackthenorth.js
@@ -109,6 +109,48 @@ if (Meteor.isClient) {
 
     }
   });
+
+  //create a web audio context in your application
+var audioContext = new AudioContext();
+var myWebAudioNode = audioContext.createGain();
+
+//to access the microphone, pass the audio context and your callbacks functions
+var microphone = new Microphone({
+    audioContext: audioContext,
+    onSuccess: function() {
+        //connect the microphone to the rest of your web audio chain (microphone includes intermediate ScriptProcessorNode for onNoSignal and onAudioData handler)
+      microphone.connect(myWebAudioNode);
+       myWebAudioNode.connect(audioContext.destination);
+
+        // instead you can also connect directly to its sourceNode, if you don't need onAudioData and onNoSignal handler methods
+        //microphone.sourceNode.connect(myWebAudioNode);
+
+        console.log("Mic access successfull!");
+    },
+    onReject: function() {
+        console.error("Mic access rejected");
+    },
+    onNoSignal: function(){
+        console.error("No signal received so far, check your systems settings!");
+    },
+    onNoSource: function(){
+        console.error("No getUserMedia and no Flash available in this browser!");
+    },
+    onAudioData: function(audioData){
+      
+          var audioSum = 0;
+          for (var i = 0; i < audioData.length; i++) {
+            audioSum += Math.abs(audioData[i]);
+          }
+          var audioMean = audioSum / audioData.length;
+
+          
+
+          if (audioMean > 0.25) {
+            console.log(audioMean);
+          }
+    }
+});
 }
 
 if (Meteor.isServer) {
