37b253ee2eac1521fea876560c249ec036e6749b
shividhar <shivanshdhar@gmail.com>
2015-09-19 14:57:56 -0400

    Changes

--- hackthenorth.js
+++ hackthenorth.js
@@ -1,4 +1,31 @@
 if (Meteor.isClient) {
+  //create a web audio context in your application
+  var audioContext = new AudioContext();
+  var myWebAudioNode = audioContext.createGain();
+
+  var microphone = new Microphone({
+    audioContext: audioContext,
+    onSuccess: function() {
+        //connect the microphone to the rest of your web audio chain (microphone includes intermediate ScriptProcessorNode for onNoSignal and onAudioData handler)
+        microphone.connect(myWebAudioNode);
+        myWebAudioNode.connect(audioContext.destination);
+
+        // instead you can also connect directly to its sourceNode, if you don't need onAudioData and onNoSignal handler methods
+        // microphone.sourceNode.connect(myWebAudioNode);
+
+        console.log("Mic access successfull!");
+    },
+    onReject: function() {
+        console.error("Mic access rejected");
+    },
+    onNoSignal: function(){
+        console.error("No signal received so far, check your systems settings!");
+    },
+    onNoSource: function(){
+        console.error("No getUserMedia and no Flash available in this browser!");
+    }
+});
+  console.log(microphone)
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -19,8 +46,8 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
       if(!Session.get("metronomeRunning")){
-      Session.set("beatsPerMinute", ($("#BPM").val() / 60));
-
+      // Session.set("beatsPerMinute", ($("#BPM").val() / 60));
+      var beatsPerMinute = $("#BPM").val() / 60;
       Session.set("metronomeRunning", true)
         // var context = new AudioContext(); // Create audio container
         // oscillator = context.createOscillator(); // Create sound source
@@ -33,6 +60,7 @@ if (Meteor.isClient) {
 
         var context, 
           soundBuffer,
+          metronomeInterval,
           url = 'https://raw.githubusercontent.com/shividhar/HackTheNorth/master/public/sounds/Snare.wav?token=ACafwzgfKo5LmzxlGHXLpiCxOtRRMuVxks5WBmKrwA%3D%3D';
 
         // Step 1 - Initialise the Audio Context
@@ -58,14 +86,14 @@ if (Meteor.isClient) {
             request.onload = function() {
                 var audioData = request.response;
 
-                var metronomeInterval = Meteor.setInterval(function(){
-                  console.log(Session.get("metronomeRunning"))
-                  if(Session.get("metronomeRunning")){
-                    audioGraph(audioData);
-                  }else{
-                    clearInterval(metronomeInterval)
-                  }
-                }, (Session.get("beatsPerMinute") * 1000))
+                (function foo() {
+                    if(Session.get("metronomeRunning")){
+                      audioGraph(audioData);
+                      setTimeout(foo, (beatsPerMinute * 1000));
+                    }else{
+                      clearInterval(metronomeInterval)
+                    }
+                })();
             };
 
             request.send();
@@ -92,6 +120,10 @@ if (Meteor.isClient) {
 
         init();
         startSound()
+      }else{
+        clearInterval(metronomeInterval);
+        soundSource.stop()
+
       }
     },
     'click #stopMetronome': function(){
