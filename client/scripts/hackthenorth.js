ed8856b7de6af7c2e5b6a9ed831f12ab2c8e9b47
AY MB <assafyossifoff@v1021-wn-33-228.campus-dynamic.uwaterloo.ca>
2015-09-20 06:04:22 -0400

    game works ish

--- client/scripts/hackthenorth.js
+++ client/scripts/hackthenorth.js
@@ -105,10 +105,59 @@ if (Meteor.isClient) {
       Session.set("metronomeRunning", false)
     }
   });
-}
+
+    //create a web audio context in your application
+var audioContext = new AudioContext();
+var myWebAudioNode = audioContext.createGain();
+
+//to access the microphone, pass the audio context and your callbacks functions
+var microphone = new Microphone({
+    audioContext: audioContext,
+    onSuccess: function() {
+        //connect the microphone to the rest of your web audio chain (microphone includes intermediate ScriptProcessorNode for onNoSignal and onAudioData handler)
+        microphone.connect(myWebAudioNode);
+        myWebAudioNode.connect(audioContext.destination);
+
+        // instead you can also connect directly to its sourceNode, if you don't need onAudioData and onNoSignal handler methods
+        // microphone.sourceNode.connect(myWebAudioNode);
+
+        console.log("Mic access successfull!");
+    },
+    onReject: function() {
+        console.error("Mic access rejected");
+    },
+    onNoSignal: function(){
+        console.error("No signal received so far, check your systems settings!");
+    },
+    onNoSource: function(){
+        console.error("No getUserMedia and no Flash available in this browser!");
+    },
+    onAudioData: function(audioData){
+        var absSum = 0;
+        for (var i = 0; i < audioData.length; ++i) {
+          absSum += Math.abs(audioData[i]);
+        }
+        var absMean = absSum / audioData.length;
+        if (absMean > 0.25) {
+          var elt = document.getElementById("example_block");
+          var currentLeft = elt.style.left;
+          var leftText = (currentLeft.substring(0, 3));
+          var finalLeft = parseInt(leftText);
+          //console.log(finalLeft);
+          if (finalLeft >= 450 && finalLeft <= 540) {
+            ++score;
+            document.getElementById("score").innerHTML = "Score : " + score;
+          }
+          //console.log(absMean);
+        }
+    }
+});
+
+  
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
 }
